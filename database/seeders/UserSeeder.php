910136ae647d76d2f61b7848057f2d83fa573702
JohanesMarioPL <113165097+Rsyntax04@users.noreply.github.com>
2026-07-08 08:15:28 +0700

    Commit changes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,71 +4,56 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Role;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        $adminRole = Role::where('role_name', 'admin')->first();
-        $moRole = Role::where('role_name', 'mo')->first();
-        $dosenRole = Role::where('role_name', 'dosen')->first();
-        $mahasiswaRole = Role::where('role_name', 'mahasiswa')->first();
+        User::create([
+            'name' => 'Budi Santoso, S.T., M.Kom.',
+            'email' => 'budi.santoso@maranatha.edu',
+            'password' => Hash::make('password'),
+            'role_id' => '3',
+            'identitas' => '720307',
+        ]);
 
-        User::firstOrCreate(
-            ['email' => 'admin@mail.com'],
-            [
-                'name' => 'Admin',
-                'identitas' => 'ADM001',
-                'password' => Hash::make('password'),
-                'role_id' => $adminRole->id,
-                'is_active' => 1,
-            ]
-        );
+        User::create([
+            'name' => 'Caca Andika',
+            'email' => 'caca@maranatha.edu',
+            'password' => Hash::make('password'),
+            'role_id' => '4',
+            'identitas' => '2272001',
+        ]);
+        
+        User::create([
+            'name' => 'Doni Prasetyo',
+            'email' => 'doni.prasetyo@maranatha.edu',
+            'password' => Hash::make('password'),
+            'role_id' => '4',
+            'identitas' => '2272002',
+        ]);
 
-        User::firstOrCreate(
-            ['email' => 'mo@mail.com'],
-            [
-                'name' => 'MO Staff',
-                'identitas' => 'MO001',
-                'password' => Hash::make('password'),
-                'role_id' => $moRole->id,
-                'is_active' => 1,
-            ]
-        );
+        User::create([
+            'name' => 'Risa, S.Kom., M.T.',
+            'email' => 'risa@maranatha.edu',
+            'password' => Hash::make('password'),
+            'role_id' => '3',
+            'identitas' => '720319',
+        ]);
 
-        User::firstOrCreate(
-            ['email' => 'dosen@mail.com'],
-            [
-                'name' => 'Dosen User',
-                'identitas' => 'DSN001',
-                'password' => Hash::make('password'),
-                'role_id' => $dosenRole->id,
-                'is_active' => 1,
-            ]
-        );
+        User::create([
+            'name' => 'Admin Program Studi',
+            'email' => 'admin@maranatha.edu',
+            'password' => Hash::make('password'),
+            'role_id' => '1',
+        ]);
 
-        User::firstOrCreate(
-            ['email' => 'mahasiswa@mail.com'],
-            [
-                'name' => 'Mahasiswa User',
-                'identitas' => 'MHS001',
-                'password' => Hash::make('password'),
-                'role_id' => $mahasiswaRole->id,
-                'is_active' => 1,
-            ]
-        );
-
-        User::firstOrCreate(
-            ['email' => 'nonaktif@mail.com'],
-            [
-                'name' => 'Mahasiswa Nonaktif',
-                'identitas' => 'MHS002',
-                'password' => Hash::make('password'),
-                'role_id' => $mahasiswaRole->id,
-                'is_active' => 0,
-            ]
-        );
+        User::create([
+            'name' => 'Manajer Operasional',
+            'email' => 'mo@maranatha.edu',
+            'password' => Hash::make('password'),
+            'role_id' => '2',
+        ]);
     }
 }
